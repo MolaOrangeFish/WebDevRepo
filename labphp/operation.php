fbd99bff8c9e9f4184848c6134ad911f44f04451
MolaOrangeFish <kittichetadulroj@gmail.com>
2022-10-04 01:25:50 +0700

    (php+db workshop 6 7 8 9)+ sql

---
+++ labphp/operation.php
@@ -0,0 +1,8 @@
+<?php
+    $a = $_GET["A"];
+    $b = $_GET["B"];
+    echo $a+$b."<br>";
+    echo $a-$b."<br>";
+    echo $a*$b."<br>";
+    echo $a/$b."<br>";
+?>
---
+++ labphp/test.php
@@ -0,0 +1,6 @@
+<?php
+  $country = $_GET["country"];
+  $language = $_GET["lang"];
+  echo "You are from " .$country. ".<br>";
+  echo "You perferred " .$language." language.";
+?>
